40e081d27c75fd1227fed7c935244a4b4eaea814
umpoohg <32526725+umpoohg@users.noreply.github.com>
2024-07-14 20:57:54 -0400

    modified:   contact.html 	modified:   contact_email.php 	modified:   donation.html 	modified:   element-team.html 	modified:   faq.html

--- contact_email.php
+++ contact_email.php
@@ -7,7 +7,7 @@ if (!isset($_REQUEST['safety_key'])) {
 
 // Admin Email.
 
-$to = "youremail@gmail.com"; // write you email address in here.
+$to = "planetplasticless@gmail.com"; // write you email address in here.
 // Fetching Values from URL.
 
 $user_name = $_POST['user_name'];
